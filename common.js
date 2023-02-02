409a4973ac9d2a58b63f4a3059535bfc7b63cac0
charlierobin <charlie@charlierobin.com>
2023-02-02 18:43:46 +0100

    created common js

---
+++ common.js
@@ -0,0 +1,42 @@
+function selectRandomPics()
+{
+    let max = 279;
+            
+    let selected = [];
+    
+    for ( var i = 0; i < 8; i++ )
+    {
+        let x = Math.floor( ( Math.random() * max ) + 1 );
+          
+          while( selected.includes( x ) )
+          {
+              x = Math.floor( ( Math.random() * max ) + 1 );
+          }
+          
+          selected.push( x );
+    }
+
+    return selected;
+}
+
+function isMobileSafari() 
+{
+    return navigator.userAgent.match(/(iPod|iPhone|iPad)/) && navigator.userAgent.match(/AppleWebKit/)
+}
+
+function getUrlGetParams()
+{
+    var vars = {};
+    
+    var hashes = window.location.href.slice( window.location.href.indexOf( '?' ) + 1 ).split( '&' );
+    
+    for ( var i = 0; i < hashes.length; i++ )
+    {
+        let hash = hashes[ i ].split( '=' );
+
+        vars[ hash[ 0 ] ] = hash[ 1 ];
+    }
+    
+    return vars;
+}
+
