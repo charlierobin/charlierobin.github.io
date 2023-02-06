734eed7058799298302e89e9179ad6c82ba67d69
charlierobin <charlie@charlierobin.com>
2023-02-06 09:25:32 +0100

    namespaced the common functions

--- common.js
+++ common.js
@@ -1,37 +1,43 @@
-function selectRandomPics()
-{
-    let max = 279;
-            
-    let selected = [];
-    
-    for ( var i = 0; i < 8; i++ )
-    {
-        let x = Math.floor( ( Math.random() * max ) + 1 );
-          
-          while( selected.includes( x ) )
-          {
-              x = Math.floor( ( Math.random() * max ) + 1 );
-          }
-          
-          selected.push( x );
+var com = {
+
+    charlierobin: {
+
+        selectRandomPics: function () 
+        {
+            let max = 279;
+
+            let selected = [];
+
+            for (var i = 0; i < 8; i++) 
+            {
+
+                let x = Math.floor((Math.random() * max) + 1);
+
+                while (selected.includes(x)) 
+                {
+                    x = Math.floor((Math.random() * max) + 1);
+                }
+
+                selected.push(x);
+            }
+
+            return selected;
+        },
+
+        getUrlGetParams: function () 
+        {
+            var vars = {};
+
+            var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+
+            for (var i = 0; i < hashes.length; i++) 
+            {
+                let hash = hashes[i].split('=');
+
+                vars[hash[0]] = hash[1];
+            }
+
+            return vars;
+        }
     }
-
-    return selected;
-}
-
-function getUrlGetParams()
-{
-    var vars = {};
-    
-    var hashes = window.location.href.slice( window.location.href.indexOf( '?' ) + 1 ).split( '&' );
-    
-    for ( var i = 0; i < hashes.length; i++ )
-    {
-        let hash = hashes[ i ].split( '=' );
-
-        vars[ hash[ 0 ] ] = hash[ 1 ];
-    }
-    
-    return vars;
-}
-
+};
